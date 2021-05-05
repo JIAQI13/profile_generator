23e02dfa737b79883ed4829946d822301547147c
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-05 04:04:33 +0000

    survey.js finished

---
+++ survery.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let name = '';
+let activity = '';
+let music = '';
+let meal = '';
+let food = '';
+let sport = '';
+let superpower = '';
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
+  name = answer;
+  rl.close();
+});
+rl.question("What's an activity you like doing? ", (answer) => {
+    activity = answer;
+    rl.close();
+  });
+  rl.question("What do you listen to while doing that? ", (answer) => {
+    music = answer;
+    rl.close();
+  });
+  rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer) => {
+    meal = answer;
+    rl.close();
+  });
+  rl.question("What's your favourite thing to eat for that meal? ", (answer) => {
+    food = answer;
+    rl.close();
+  });
+  rl.question("Which sport is your absolute favourite? ", (answer) => {
+    sport = answer;
+    rl.close();
+  });
+  rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer) => {
+    superpower = answer;
+    rl.close();
+  });
+
+  console.log(`${name} loves listening to ${music} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
